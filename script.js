e4895fc1e26ef76caa986794e6132301d0107cc7
Katie Farris <klynnfarris@gmail.com>
2021-06-15 21:39:43 -0500

    final files added

---
+++ script.js
@@ -0,0 +1,24 @@
+window.addEventListener("load", function(){
+fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+console.log(response);
+response.json().then(function(json) {
+    const destination = document.getElementById("container");
+    for (index = 0; index < json.length; index++){
+       destination.innerHTML += `
+          <div class= "astronaut">
+            <div class = "bio">
+             <h3>${json[index].firstName + " " + json[index].lastName}</h3>
+             <ul>
+             <li>Hours in space: ${json[index].hoursInSpace} </li>
+             <li>Active: ${json[index].active}</li>
+             <li>Skills: ${json[index].skills}</li>
+             </ul>
+             
+          </div>
+          <img class="avatar" src=${json[index].picture}>
+          </div>
+       `;
+    };
+    });
+ });
+});
